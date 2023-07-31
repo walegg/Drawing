2aab3f74341f30fb97d307182a4ce715877e1cf6
Louis Mille <mille.louis1@gmail.com>
2023-07-31 17:33:10 +0200

    rainbow circle animation

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -20,54 +20,137 @@ import SwiftUI
 //    }
 //}
 //
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockWise: Bool
-    var insetAmount = 0.0
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockWise)
-
-        return path
-    }
-
-}
-
-
-struct ContentView: View {
-    var body: some View {
-        
-        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockWise: true)
-            .stroke(.blue, lineWidth: 10)
-            .frame(width: 300, height: 300)
+//struct Arc: InsettableShape {
+//    var startAngle: Angle
+//    var endAngle: Angle
+//    var clockWise: Bool
+//    var insetAmount = 0.0
 //
-//        Triangle()
-//            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-//            .frame(width: 300, height: 300)
-//        Path { path in
-//            path.move(to: CGPoint(x: 200, y: 100))
-//            path.addLine(to: CGPoint(x: 100, y: 300))
-//            path.addLine(to: CGPoint(x: 300, y: 300))
-//            path.addLine(to: CGPoint(x: 200, y: 100))
-//            path.closeSubpath()
+//    func inset(by amount: CGFloat) -> some InsettableShape {
+//        var arc = self
+//        arc.insetAmount += amount
+//        return arc
+//    }
+//
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//        let rotationAdjustment = Angle.degrees(90)
+//        let modifiedStart = startAngle - rotationAdjustment
+//        let modifiedEnd = endAngle - rotationAdjustment
+//
+//        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockWise)
+//
+//        return path
+//    }
+//
+//}
+//struct Flower: Shape {
+//    var petalOffset = -20.0
+//    var petalWidth = 100.0
+//
+//
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//
+//        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+//            let rotation = CGAffineTransform(rotationAngle: number)
+//            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+//
+//            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+//
+//            let rotatedPetal = originalPetal.applying(position)
+//
+//            path.addPath(rotatedPetal)
+//
 //        }
-//        .fill(.blue)
-//        .stroke(.blue, lineWidth: 10)
-//        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//
+//        return path
+//    }
+//}
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(gradient: Gradient(colors: [
+                            color(for: value, brightness: 1),
+                            color(for: value, brightness: 0.5)
+                        ]),
+                        startPoint: .top,
+                        endPoint: .bottom),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
         
     }
+}
+struct ContentView: View {
+//    @State private var petalOffset = -20.0
+//    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
+    
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+            Slider(value: $colorCycle)
+        }
+        
+        
+        
+//        Text("Hello world")
+//            .frame(width: 300, height: 300)
+//            .border(ImagePaint(image: Image("example"), sourceRect:CGRect(x: 0, y: 0.25, width: 1, height: 0.5) , scale: 0.2), width: 30)
+//        VStack {
+//            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+////                .stroke(.red, lineWidth: 1)
+//                .fill(.red, style: FillStyle(eoFill: true))
+//
+//            Text("Offset")
+//            Slider(value: $petalOffset, in: -40...40)
+//                .padding([.horizontal, .bottom])
+//
+//            Text("width")
+//            Slider(value: $petalOffset, in: 0...100)
+//                .padding(.horizontal)
+//        }
+        
+        //        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockWise: true)
+        //            .stroke(.blue, lineWidth: 10)
+        //            .frame(width: 300, height: 300)
+        //
+        //        Triangle()
+        //            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        //            .frame(width: 300, height: 300)
+        //        Path { path in
+        //            path.move(to: CGPoint(x: 200, y: 100))
+        //            path.addLine(to: CGPoint(x: 100, y: 300))
+        //            path.addLine(to: CGPoint(x: 300, y: 300))
+        //            path.addLine(to: CGPoint(x: 200, y: 100))
+        //            path.closeSubpath()
+        //        }
+        //        .fill(.blue)
+        //        .stroke(.blue, lineWidth: 10)
+        //        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
