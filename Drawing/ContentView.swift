530b1273d13aa6d72b571968a29bc57a68f1ce24
Louis Mille <mille.louis1@gmail.com>
2023-08-01 18:06:50 +0200

    made animations

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -67,52 +67,157 @@ import SwiftUI
 //        return path
 //    }
 //}
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
+//struct ColorCyclingCircle: View {
+//    var amount = 0.0
+//    var steps = 100
+//
+//    var body: some View {
+//        ZStack {
+//            ForEach(0..<steps) { value in
+//                Circle()
+//                    .inset(by: Double(value))
+//                    .strokeBorder(
+//                        LinearGradient(gradient: Gradient(colors: [
+//                            color(for: value, brightness: 1),
+//                            color(for: value, brightness: 0.5)
+//                        ]),
+//                        startPoint: .top,
+//                        endPoint: .bottom),
+//                        lineWidth: 2
+//                    )
+//            }
+//        }
+//        .drawingGroup()
+//    }
+//
+//    func color(for value: Int, brightness: Double) -> Color {
+//        var targetHue = Double(value) / Double(steps) + amount
+//
+//        if targetHue > 1 {
+//            targetHue -= 1
+//        }
+//        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+//
+//    }
+//}
+//struct Trapezoid: Shape {
+//    var insetAmount: Double
+//
+//    //make withAnimation work
+//    var animatableData: Double {
+//        get { insetAmount }
+//        set { insetAmount = newValue }
+//    }
+//
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//
+//        path.move(to: CGPoint(x: 0, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+//        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+//
+//        return path
+//    }
+//}
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
     
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: Double(value))
-                    .strokeBorder(
-                        LinearGradient(gradient: Gradient(colors: [
-                            color(for: value, brightness: 1),
-                            color(for: value, brightness: 0.5)
-                        ]),
-                        startPoint: .top,
-                        endPoint: .bottom),
-                        lineWidth: 2
-                    )
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+        
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of:2) {
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
             }
         }
-        .drawingGroup()
-    }
-    
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
-        
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-        
+        return path
     }
 }
+
 struct ContentView: View {
 //    @State private var petalOffset = -20.0
 //    @State private var petalWidth = 100.0
-    @State private var colorCycle = 0.0
+//    @State private var colorCycle = 0.0
+//    @State private var amount = 0.0
+//    @State private var insetAmount = 0.0
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
-        VStack {
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
-            Slider(value: $colorCycle)
-        }
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
+                }
+            }
+//        Trapezoid(insetAmount: insetAmount)
+//            .frame(width: 200, height: 100)
+//            .onTapGesture {
+//                withAnimation {
+//                    insetAmount = Double.random(in: 10...90)
+//                }
+//            }
+//        VStack {
+//            ZStack {
+//                ZStack {
+//                    Circle()
+//                        .fill(Color(red: 1, green: 0, blue: 0))
+//                        .frame(width: 200 * amount)
+//                        .offset(x: -50, y: -100)
+//                        .blendMode(.screen)
+//                    Circle()
+//                        .fill(Color(red: 0, green: 1, blue: 0))
+//                        .frame(width: 200 * amount)
+//                        .offset(x: 50, y: -100)
+//                        .blendMode(.screen)
+//                    Circle()
+//                        .fill(Color(red: 0, green: 0, blue: 1))
+//                        .frame(width: 200 * amount)
+//                        .blendMode(.screen)
+//    //                Circle()
+//    //                    .fill(.yellow)
+//    //                    .frame(width: 200 * amount)
+//    //                    .offset(x: 0, y: -62)
+//    //                    .blendMode(.screen)
+//                    Slider(value: $amount)
+//                        .padding()
+//
+//                }
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                .background(.black)
+//                .ignoresSafeArea()
+//            }
+//        }
         
-        
+//        VStack {
+//            ColorCyclingCircle(amount: colorCycle)
+//                .frame(width: 300, height: 300)
+//            Slider(value: $colorCycle)
+//        }
+
         
 //        Text("Hello world")
 //            .frame(width: 300, height: 300)
